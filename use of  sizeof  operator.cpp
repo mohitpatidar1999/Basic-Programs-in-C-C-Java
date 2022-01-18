a0a1f78758c31613b1e71d5b377a60c21eb30871
Mohit Patidar <56833571+mohitpatidar1999@users.noreply.github.com>
2022-01-18 23:05:43 +0530

    Add files via upload

---
+++ use of  sizeof  operator.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("%d",sizeof(int));
+	printf("\n%d",sizeof(char));
+	printf("\n%d",sizeof(float));
+	printf("\n%d",sizeof(double));
+	printf("\n%d",sizeof(long int));
+	printf("\n%d",sizeof(long double));
+	return(0);
+	getch();
+}
